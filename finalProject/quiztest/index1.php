1ac297db5224b9a8b2b79cd9f3e74a61738e8646
masha-krotkykh <31903446+masha-krotkykh@users.noreply.github.com>
2018-11-25 19:02:47 -0500

    attribution added

--- finalProject/quiztest/index1.php
+++ finalProject/quiztest/index1.php
@@ -295,9 +295,12 @@
       }
     }
 
-
+// Function to enable SVG image dynamic properties change
+// Borrowed from StackOverflow
+// https://stackoverflow.com/questions/24933430/img-src-svg-changing-the-fill-color?answertab=votes#tab-top
+// http://jsfiddle.net/wuSF7/462/
     $(function(){
-    jQuery('img.svg').each(function(){
+      jQuery('img.svg').each(function(){
         var $img = jQuery(this);
         var imgID = $img.attr('id');
         var imgClass = $img.attr('class');
@@ -306,7 +309,6 @@
         jQuery.get(imgURL, function(data) {
             // Get the SVG tag, ignore the rest
             var $svg = jQuery(data).find('svg');
-
             // Add replaced image's ID to the new SVG
             if(typeof imgID !== 'undefined') {
                 $svg = $svg.attr('id', imgID);
@@ -315,22 +317,18 @@
             if(typeof imgClass !== 'undefined') {
                 $svg = $svg.attr('class', imgClass+' replaced-svg');
             }
-
             // Remove any invalid XML tags as per http://validator.w3.org
             $svg = $svg.removeAttr('xmlns:a');
-
             // Check if the viewport is set, else we gonna set it if we can.
             if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
                 $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
             }
-
             // Replace image with new SVG
             $img.replaceWith($svg);
 
         }, 'xml');
-
+      });
     });
-});
 
      </script>
     </body>
